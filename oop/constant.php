6cff1263a18d3f508e607edb720f8a49b12b85c5
IniFadhil <rffadhil07@gmail.com>
2024-10-19 11:23:14 +0700

    vidio 16 - Autoloading

---
+++ oop/constant.php
@@ -0,0 +1,15 @@
+<?php
+// define ('NAMA', 'Rizal Fahla');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 32;
+// echo UMUR;
+
+class Coba
+{
+    const NAMA = 'Fadhil Rizki Fauzan';
+}
+
+echo Coba::NAMA;
